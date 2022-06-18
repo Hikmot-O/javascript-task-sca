d7bf73677c626adc22a599c85a083d44183668e0
hikmotokunola <hikmotokunola@gmail.com>
2022-06-18 09:12:21 +0100

    soln

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // complete the function
 function prime (num) {
   // code goes here
+  const arr = [];
+    if(num === 1 || num === 2){
+        arr.push(true);
+    }else if(num <= 0){
+        arr.push(false);    
+    }
+    else{
+        for(let i=2; i<num; i++){
+           if(num % i === 0){
+               arr.push(false);
+            }else if (num % i !== 0){
+                arr.push(true);
+            }
+        }
+    }
+    if(arr.some(el => el === false)){
+        console.log (false);
+    }else{
+        console.log (true);
+    };
 }
 
 function solution (arg) {
